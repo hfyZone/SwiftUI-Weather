7112ffcf7df659900c066b5399a694b3e8f6314e
hfyZone <1178805691@qq.com>
2025-11-23 21:44:18 +0800

    modifer 顺序对渲染结果有影响

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // 添加线性颜色背景
+            LinearGradient(
+                gradient: Gradient(colors: [.blue, .white]),
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing
+            ).ignoresSafeArea()
+            VStack {
+                // 修饰器的顺序影响渲染结果，比如background在frame前修饰文字，在之后修饰frame。
+                Text("济南，槐荫区")
+                    .font(.system(size: 32,weight: .medium,design: .default))
+                    .foregroundStyle(.white)
+                    .frame(width: 200, height: 200)
+                    .background(.red)
+            }
         }
-        .padding()
     }
 }
 
