9e6ac4fbe91cf89ef6452b784c1435440315ba56
hfyZone <1178805691@qq.com>
2025-11-24 00:01:55 +0800

    1、复用代码 2、 自定义颜色 3、 use padding to replace Spacer()

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -12,34 +12,67 @@ struct ContentView: View {
         ZStack {
             // 添加线性颜色背景
             LinearGradient(
-                gradient: Gradient(colors: [.blue, .white]),
+                gradient: Gradient(colors: [.blue, Color("LightBlue")]),
                 startPoint: .topLeading,
                 endPoint: .bottomTrailing
             ).ignoresSafeArea()
-            VStack(spacing: 10) {
+            VStack {
                 // 修饰器的顺序影响渲染结果，比如background在frame前修饰文字，在之后修饰frame。
                 Text("济南，槐荫区")
                     .font(.system(size: 32, weight: .medium, design: .default))
                     .foregroundStyle(.white)
                     .padding()
-                VStack{
+                VStack(spacing: 10){
                     Image(systemName: "cloud.sun.fill")
                         .renderingMode(.original)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
                         .frame(width: 180, height: 180)
+                    Text("26°C")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundStyle(.white)
                 }
-                Text("26°C")
-                    .font(.system(size: 70, weight: .medium))
-                    .foregroundStyle(.white)
+                .padding(.bottom, 40)
+                
+                HStack(spacing: 15) {
+                    WeatherDayView(dayOfWeek: "周一", imageName: "cloud.sun.fill", temperature: 24)
+                    WeatherDayView(dayOfWeek: "周二", imageName: "cloud.fill", temperature: 20)
+                    WeatherDayView(dayOfWeek: "周三", imageName: "cloud.fill", temperature: 21)
+                    WeatherDayView(dayOfWeek: "周四", imageName: "cloud.rain.fill", temperature: 19)
+                    WeatherDayView(dayOfWeek: "周五", imageName: "cloud.rain.fill", temperature: 22)
+                    WeatherDayView(dayOfWeek: "周六", imageName: "sun.max.fill", temperature: 28)
+                }
                 Spacer()
+                
             }
-            
-            
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct WeatherDayView: View {
+    var dayOfWeek: String
+    var imageName: String
+    var temperature: Int
+    
+    var body: some View {
+        VStack {
+            Text(dayOfWeek)
+                .font(.system(size: 20, weight: .medium))
+                .foregroundStyle(.white)
+            
+            Image(systemName: imageName)
+                .renderingMode(.original)
+                .resizable()
+                .aspectRatio(contentMode: ContentMode.fit)
+                .frame(width: 40, height: 40)
+            Text("\(temperature)°C")
+                .font(.system(size: 20, weight: .medium))
+                .foregroundStyle(.white)
+        }
+    }
+
 }
